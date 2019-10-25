114d46487519c86c7b26907a7f0c163d68578230
清与时光 <97351832@qq.com>
2019-10-25 19:37:13 +0800

    换个机器继续写

---
+++ src/pages/home/cpmponents/Recommend.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-imgwrapper">
+          <img class="item-img" src />
+        </div>
+        <div class="item-info">
+          <p class="item-title"></p>
+          <p class="item-describe"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style lang="stylus" scoped>
+.recommend-title
+  margin-top: 0.2rem
+  line-height: 0.8rem
+  background: #eee
+  text-indent: 0.2rem
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  background: red
+</style>
