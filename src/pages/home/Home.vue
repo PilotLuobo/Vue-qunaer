b70df757a835c86cebc2c921df9ee6f86759d21d
清与时光 <97351832@qq.com>
2019-10-25 09:55:39 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './cpmponents/Header'
+import HomeSwiper from './cpmponents/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/cpmponents/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '01',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+      },
+      {
+        id: '02',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/44/b503d91fd879d102.jpg_750x200_8e21a135.jpg'
+      }]
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  width: 100%
+  height: 0
+  overflow: hidden
+  padding-bottom: 26.67%
+  background: #ccc
+  .swiper-img
+    width: 100%
+</style>
